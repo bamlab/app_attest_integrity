b42a8c96c7c1c9f1270a34d862c29dad84f1b95e
Louis Giboin <louis.giboin@theodo.com>
2025-08-14 17:33:47 +0200

    feat(android): use pigeon method

--- android/src/main/kotlin/com/theodo/app_attest_integrity/AppAttestIntegrityPlugin.kt
+++ android/src/main/kotlin/com/theodo/app_attest_integrity/AppAttestIntegrityPlugin.kt
@@ -1,33 +1,26 @@
 package com.theodo.app_attest_integrity
 
+import AppAttestIntegrityApi
+import android.content.Context
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
-/** AppAttestIntegrityPlugin */
-class AppAttestIntegrityPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class AppAttestIntegrityPlugin: FlutterPlugin, AppAttestIntegrityApi {
+  private var context: Context? = null
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "app_attest_integrity")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
+    AppAttestIntegrityApi.setUp(flutterPluginBinding.binaryMessenger, this)
+    context = flutterPluginBinding.applicationContext
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+    AppAttestIntegrityApi.setUp(binding.binaryMessenger, null)
+    context = null
   }
+
+  override fun getPlatformVersion(): String = "Android ${android.os.Build.VERSION.RELEASE}"
+
 }
